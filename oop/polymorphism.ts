bc4abb18b0ccad2d0c48e18a55e72bf4f359b843
Esmail Elmoussel <elmoussel12@gmail.com>
2023-09-06 01:49:53 +0300

    add OOP polymorphism example

---
+++ oop/polymorphism.ts
@@ -0,0 +1,40 @@
+export {};
+
+// Polymorphism allows objects of different classes to be treated as objects of a common superclass.
+// It enables the implementation of generic code that can work with objects of
+// various types, promoting flexibility and extensibility.
+class Animal {
+  constructor(private name: string) {}
+
+  speak(): void {
+    console.log(
+      `I am a ${this.name} and this is a generic implementation of speak method!`
+    );
+  }
+}
+
+class Dog extends Animal {
+  constructor() {
+    super("Dog");
+  }
+
+  speak(): void {
+    console.log("Whoofff!!!");
+  }
+}
+
+class Cat extends Animal {
+  constructor() {
+    super("Cat");
+  }
+
+  speak(): void {
+    console.log("Meowww!!!");
+  }
+}
+
+const animal1: Animal = new Dog();
+const animal2: Animal = new Cat();
+
+animal1.speak();
+animal2.speak();
